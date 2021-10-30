b3adce2f1b1537571e1853e7ff644b4a14ab08ef
Bardia <bardiafreelancing@gmail.com>
2021-10-30 20:21:45 +0330

    TTF changed to ttf

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-// import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/general/layout";
 import Landing from "../components/exclusive/homePage/landing";
