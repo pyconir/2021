855dfee6d6d82b300f8ce7692ed04cc2841abebf
Bardia <bardiafreelancing@gmail.com>
2021-11-03 23:08:56 +0330

    rel=noopener added to target_blank

--- components/exclusive/aidPage/aidPage.js
+++ components/exclusive/aidPage/aidPage.js
@@ -12,6 +12,7 @@ export default function AidPage() {
             <a
               href="https://pycon.org/"
               target="_blank"
+              rel="noopener noreferrer"
               style={{ textDecoration: "none" }}
             >
               PyCon
