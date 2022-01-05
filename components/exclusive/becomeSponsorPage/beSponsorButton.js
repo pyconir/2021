5a2be94d726567a89883fd15607eb7d4cb45a6f1
python-irpycon <pyconforall@protonmail.com>
2022-01-05 16:25:45 +0330

    some aid page imporvements

--- components/exclusive/becomeSponsorPage/beSponsorButton.js
+++ components/exclusive/becomeSponsorPage/beSponsorButton.js
@@ -5,7 +5,7 @@ export default function BeSponsorButton() {
   const { t } = useTranslation();
   const [query] = useLanguageQuery();
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div style={{ display: "flex", justifyContent: "center", marginTop:"40px" }}>
       <a style={{textDecoration:"none"}} href="mailto:team@ir.pycon.org">
         <div className={styles.beSponsorButton + " " + "unSelectable"}>
           {t("pages.becomeSponsor.button")}
--- components/exclusive/jobOpportunitiesPage/sendJobButton.js
+++ components/exclusive/jobOpportunitiesPage/sendJobButton.js
@@ -1,9 +1,28 @@
 import styles from "./SendJobButton.module.css";
+import { useTranslation, useLanguageQuery } from "next-export-i18n";
+import Link from "next/link";
 
 export default function SendJobButton() {
+  const { t } = useTranslation();
+  const [query] = useLanguageQuery();
+  const pagelang = () => {
+    if (typeof query !== "undefined") {
+      if (query.lang == "en") {
+        return "en";
+      } else {
+        return "fa";
+      }
+    } else {
+      return "fa";
+    }
+  };
   return (
-    <div className={styles.SendJobButtonButtonContainer + " " + "buttonsText"}>
-      <div>ارسال فرصت شغلی</div>
-    </div>
+    <Link href={{ pathname: "/become-a-sponsor", query: query }}>
+      <div
+        className={styles.SendJobButtonButtonContainer + " " + "buttonsText"}
+      >
+        <div>{t("pages.jobOpportunities.page.button")}</div>
+      </div>
+    </Link>
   );
 }
